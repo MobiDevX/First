b778ad12b6b821232aa0296fe94745bcfb0c066d
MobDe <MobDevX.outlook.com>
2022-01-20 17:08:53 +0700

    Код для RadioGroup

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     lateinit var editText: EditText
     lateinit var result: TextView
+    // Добавление радиогруппы с радиокнопками
+    lateinit var radioGroup: RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,8 @@ class MainActivity : AppCompatActivity() {
         editText = findViewById(R.id.editText)
         // Добавление слушателя для редактируемого поля
         editText.addTextChangedListener(textWatcher)
+        // Связывание переменной radioGroup с макетом радиогруппы
+        radioGroup = findViewById(R.id.radioGroup)
     }
 
     private val textWatcher = object : TextWatcher {
@@ -27,8 +32,17 @@ class MainActivity : AppCompatActivity() {
         override fun beforeTextChanged(str: CharSequence?, start: Int, count: Int, after: Int) {
         }
         override fun onTextChanged(str: CharSequence?, start: Int, before: Int, count: Int) {
-            result.text = (str.toString().toDouble()*.7112).toString()
+            val idRadioButton = radioGroup.checkedRadioButtonId
+            val k: Double = when(idRadioButton) {
+                R.id.radioButton1 -> 0.7112
+                R.id.radioButton2 -> 1066.8
+                R.id.radioButton3 -> 2.134
+                R.id.radioButton4 -> 0.42
+                R.id.radioButton5 -> 0.178
+                else -> 1.0
+            }
+            val number = str.toString().toDouble()*k
+            result.text = "%.3f".format(number)
         }
     }
 }
-
