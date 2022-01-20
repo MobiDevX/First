ae5516d137676777472ba036a6aee7ad282e85a0
MobDe <MobDevX.outlook.com>
2022-01-20 13:53:08 +0700

    Добавление OnClickListener

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.first
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     lateinit var editText: EditText
     lateinit var result: TextView
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +18,12 @@ class MainActivity : AppCompatActivity() {
 
         editText = findViewById(R.id.editText)
         result = findViewById(R.id.result)
-    }
-
-    fun converter(v: View) {
-        val str = editText.text.toString()
-        result.text = (str.toDouble()*.7112).toString()
+        button = findViewById(R.id.button)
+        // Интерфейс слушателя для кнопки
+        button.setOnClickListener{
+            val str = editText.text.toString()
+            result.text = (str.toDouble()*.7112).toString()
+        }
     }
 }
 
